154ba5999f73edae10e84add7ca74ace26aed5fa
adamjralph <adamralph@posteo.net>
2022-01-25 14:59:23 +1100

    set up basic html, JS and CSS - working

---
+++ index.js
@@ -0,0 +1,7 @@
+/* PROTOJS 
+* A prototype expense tracking app
+* By Adam Ralph */
+
+// Test 
+console.log(document.getElementById('heading'));
+document.getElementById('heading').style.color = 'red';
