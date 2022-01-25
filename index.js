6f04c3505c12143de6ac180209017801f3017663
adamjralph <adamralph@posteo.net>
2022-01-25 23:20:50 +1100

    basic test setup working

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-/* PROTOJS 
+/* PROTOJS
 * A prototype expense tracking app
 * By Adam Ralph */
 
 // Test 
-// console.log(document.getElementById('heading'));
-// const heading = document.getElementById('heading');
-export default function add(x, y) {
-  return x + y;
-}
+let val;
+
+const heading = document.getElementById('heading');
+const itemLabel = document.getElementById('item');
+
+
+
+val = itemLabel;
+
+console.log(val);
+// const form = document.querySelector('form');
+// const formGroup = form.querySelector('input-group')
+// const itemLabel = formGroup.querySelector('item-label');
+// console.log(formGroup);
+
+export {
+  heading,
+  itemLabel
+};
--- tests.js
+++ tests.js
@@ -1,7 +1,28 @@
-import selectHeading from './index.js';
+import { heading, itemLabel } from './index.js';
 
-function testSelection(selection) {
-  if selection = 
+function testResult(result) {
+  if (result === 'pass') {
+    console.log('\x1b[32m%s\x1b[0m', 'PASSED');  //green
+  } else {
+    console.log('\x1b[31m%s\x1b[0m', 'FAILED');  //red
+  }
 }
+
+function testSelectors(selector, expectedResult) {
+  console.log(selector);
+  console.log(`Testing: ${selector} ...`)
+  console.log(`Expecting: ${expectedResult}`)
+  if (selector === expectedResult) {
+    let result = testResult('pass');
+  } else {
+    let result = testResult('fail');
+  }
+}
+
+console.log(itemLabel.id);
+
+
+testSelectors(heading.id, 'heading');
+testSelectors(itemLabel.id, 'item');
 
 
